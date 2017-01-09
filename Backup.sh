f5f103a456d6ffa8b225144bd8a3f3b3d4e13a01
Flashbang007 <Flashbang007@gmail.com>
2017-01-09 13:00:41 +0100

    Update Backup.sh

--- Backup.sh
+++ Backup.sh
@@ -2,16 +2,17 @@
 # Skript zur estellung eines Backups meiner wichtigen Dateien
 
 # ------Einstellungen------
-BACKUPDIR=media/HDD/Backup
+BACKUPDIR=media/Backup
 SOURCE=media/NAS/NAS/
-EXCLUDE="--exclude=media/NAS/NAS/Download --exclude=media/NAS/NAS/Filme --exclude=media/NAS/NAS/Serien"
+EXCLUDE="--exclude=media/NAS/NAS/Filme --exclude=media/NAS/NAS/Serien"
 DATUM="$(date -I)"
+MAILUSER=flashbang
 
 cd /
 
 if [ ! -d "${SOURCE}" ]; then
 
-mail -s "NAS nicht gemountet oderVerzeichnis nicht vorhanden" root <<EOM
+mail -s "NAS nicht gemountet oderVerzeichnis nicht vorhanden" $MAILUSER <<EOM
 ${SOURCE} ist aus irgend einem Grund nicht vorhanden.
 EOM
 
@@ -21,7 +22,7 @@ else
 
 if [ ! -d "${BACKUPDIR}" ] ; then
 
-mail -s "Backup-Festplatte nicht gemountet oder Verzeichnis nicht vorhanden" root <<EOM
+mail -s "Backup-Festplatte nicht gemountet oder Verzeichnis nicht vorhanden" $MAILUSER <<EOM
 ${BACKUPDIR} ist aus irgend einem Grund nicht vorhanden.
 EOM
 
@@ -43,14 +44,16 @@ fi
 
 if [ $? -ne 0 ]; then
 
-mail -s "Backup war fehlerhaft!" root <<EOM
+mail -s "Backup war fehlerhaft!" $MAILUSER <<EOM
 Hallo Admin,
 das Backup in ${BACKUPDIR} am ${DATUM} wurde mit Fehler(n) beendet.
 EOM
 
+exit 5
+
 else
 
-mail -s "Backup war erfolgreich!" root <<EOM
+mail -s "Backup war erfolgreich!" $MAILUSER <<EOM
 Hallo Admin,
 das Backup in ${BACKUPDIR} am ${DATUM} wurde ohne Fehler beendet.
 EOM
